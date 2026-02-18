73b5296b5682f086d7610403c7542dc9d737706b
Norayr <norayr.babayann@gmail.com>
2026-02-18 22:55:48 +0400

    Update Verify.jsx

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -5,33 +5,33 @@ import axios from "axios";
 import { StoreContext } from '../../context/StoreContext';
 
 const Verify = () => {
-  const {backendURL} = useContext(StoreContext)
+  const { backendURL } = useContext(StoreContext);
   const [searchParams] = useSearchParams();
   const success = searchParams.get("success");
   const orderId = searchParams.get("orderId");
   const navigate = useNavigate();
 
-  const verifyPayment = async () => {
-    try {
-      const response = await axios.post(`${backendURL}/api/order/verify`, { success, orderId });
-      if (response.data.success) {
-        navigate('/userorders');
+  useEffect(() => {
+    const verify = async () => {
+      if (success && orderId) {
+        try {
+          const response = await axios.post(`${backendURL}/api/order/verify`, { success, orderId });
+          if (response.data.success) {
+            navigate('/userorders');
+          } else {
+            navigate('/');
+          }
+        } catch (error) {
+          console.error(error);
+          navigate('/');
+        }
       } else {
         navigate('/');
       }
-    } catch (error) {
-      console.error(error);
-      navigate('/');
-    }
-  };
+    };
 
-  useEffect(() => {
-    if (success && orderId) {
-      verifyPayment();
-    } else {
-      navigate('/');
-    }
-  }, [success,orderId]);
+    verify();
+  }, [success, orderId, backendURL, navigate]);
 
   return (
     <div className='verify'>
