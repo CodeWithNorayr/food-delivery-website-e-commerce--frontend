92374b3a98c561710366b85453c3c3fd53868591
Norayr <norayr.babayann@gmail.com>
2026-02-19 15:02:48 +0400

    Update Verify.jsx

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -16,7 +16,7 @@ const Verify = () => {
       const response = await axios.post(`${backendURL}/api/order/verify`, { success, orderId }, {
       });
       if (response.data.success) {
-        navigate('/userorders');
+        navigate('/');
       } else {
         navigate('/');
       }
